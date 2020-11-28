27b1dda9a04403c09c86c056942ac973a9e09f88
junghun yang <luciferkala@gmail.com>
2020-11-28 23:31:36 +0900

    Finish to create kubernetes objects

--- src/models/GroupDBManager.ts
+++ src/models/GroupDBManager.ts
@@ -12,7 +12,7 @@ class GroupDBManager extends DBManager {
             process.env.DB_PASSWORD,
             {
                 host: process.env.DB_HOST,
-                // port: 5434,
+                port: process.env.DB_PORT,
                 dialect: "postgres",
                 pool: {
                     max: 20,
