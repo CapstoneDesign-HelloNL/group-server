803b588138cc6741468eff48798e8878d7c40baf
junghun yang <luciferkala@gmail.com>
2020-12-01 15:53:51 +0900

    Remove member table4

--- src/dao/KafkaDao.ts
+++ src/dao/KafkaDao.ts
@@ -1,8 +1,8 @@
 import KafkaManager from "@src/models/KafkaManager";
 import Dao from "@src/dao/Dao";
 import { Consumer, EachMessagePayload, Producer } from "kafkajs";
-import MemberDao from "./member/MemberDao";
-import Member from "@src/models/member/MemberModel";
+// import MemberDao from "./member/MemberDao";
+// import Member from "@src/models/member/MemberModel";
 import KafkaData from "@src/vo/group/services/kafkaData";
 
 interface producers {
@@ -45,81 +45,73 @@ class KafkaDao extends Dao {
         this.messageFuncs = {
             userMemberCreate: async ({ topic, partition, message }: any) => {
                 const received: KafkaData = JSON.parse(message.value);
-                if (received.status === "Success") {
-                    const saveMemberData = { email: received.data.email };
-                    const newMember:
-                        | Member
-                        | undefined = await MemberDao.getInstance().save({
-                        data: saveMemberData
-                    });
-                    if (newMember instanceof Member) {
-                        await this.sendMessage(
-                            "memberUserCreate",
-                            "memberUserCreate",
-                            {
-                                status: "Success",
-                                data: { msg: "Member Create Success!" }
-                            }
-                        );
-                    } else {
-                        await this.sendMessage(
-                            "memberUserCreate",
-                            "memberUserCreate",
-                            {
-                                status: "Fail",
-                                data: { msg: "Member Create Fail!" }
-                            }
-                        );
-                    }
-                } else {
-                    await this.sendMessage(
-                        "memberUserCreate",
-                        "memberUserCreate",
-                        {
-                            status: "Fail",
-                            data: { msg: "User Create Fail!" }
-                        }
-                    );
-                }
+                // if (received.status === "Success") {
+                //     const saveMemberData = { email: received.data.email };
+                //     const newMember:
+                //         | Member
+                //         | undefined = await MemberDao.getInstance().save({
+                //         data: saveMemberData
+                //     });
+                //     if (newMember instanceof Member) {
+                //         await this.sendMessage(
+                //             "memberUserCreate",
+                //             "memberUserCreate",
+                //             {
+                //                 status: "Success",
+                //                 data: { msg: "Member Create Success!" }
+                //             }
+                //         );
+                //     } else {
+                //         await this.sendMessage(
+                //             "memberUserCreate",
+                //             "memberUserCreate",
+                //             {
+                //                 status: "Fail",
+                //                 data: { msg: "Member Create Fail!" }
+                //             }
+                //         );
+                //     }
+                // } else {
+                await this.sendMessage("memberUserCreate", "memberUserCreate", {
+                    status: "Fail",
+                    data: { msg: "User Create Fail!" }
+                });
+                // }
             },
             userMemberDelete: async ({ topic, partition, message }: any) => {
                 const received: KafkaData = JSON.parse(message.value);
-                if (received.status === "Success") {
-                    const deleteMemberParams = { email: received.data.email };
-                    const newMember:
-                        | number
-                        | undefined = await MemberDao.getInstance().delete({
-                        params: deleteMemberParams
-                    });
-                    if (newMember === 1) {
-                        await this.sendMessage(
-                            "memberUserDelete",
-                            "memberUserDelete",
-                            {
-                                status: "Success",
-                                data: { msg: "Member Delete Success!" }
-                            }
-                        );
-                    } else {
-                        await this.sendMessage(
-                            "memberUserDelete",
-                            "memberUserDelete",
-                            {
-                                status: "Fail",
-                                data: { msg: "Member Delete Fail!" }
-                            }
-                        );
-                    }
-                } else {
-                    await this.sendMessage(
-                        "memberUserDelete",
-                        "memberUserDelete",
-                        {
-                            status: "Fail",
-                            data: { msg: "User Delete Fail!" }
-                        }
-                    );
-                }
+                // if (received.status === "Success") {
+                //     const deleteMemberParams = { email: received.data.email };
+                //     const newMember:
+                //         | number
+                //         | undefined = await MemberDao.getInstance().delete({
+                //         params: deleteMemberParams
+                //     });
+                //     if (newMember === 1) {
+                //         await this.sendMessage(
+                //             "memberUserDelete",
+                //             "memberUserDelete",
+                //             {
+                //                 status: "Success",
+                //                 data: { msg: "Member Delete Success!" }
+                //             }
+                //         );
+                //     } else {
+                //         await this.sendMessage(
+                //             "memberUserDelete",
+                //             "memberUserDelete",
+                //             {
+                //                 status: "Fail",
+                //                 data: { msg: "Member Delete Fail!" }
+                //             }
+                //         );
+                //     }
+                // } else {
+                await this.sendMessage("memberUserDelete", "memberUserDelete", {
+                    status: "Fail",
+                    data: { msg: "User Delete Fail!" }
+                });
+                // }
             }
         };
         const firstInit = async () => await this.init();
